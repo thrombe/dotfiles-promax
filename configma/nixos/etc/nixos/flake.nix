1fd08d0d5798f5851ebe94878852079e37171a16
thrombe <yash.n.thombare620@gmail.com>
2023-11-07 02:52:16 +0530

    disable du-dust shell completion

--- configma/nixos/etc/nixos/flake.nix
+++ configma/nixos/etc/nixos/flake.nix
@@ -68,6 +68,9 @@
         (self: super: {
           helix = flakeDefaultPackage inputs.helix-git;
           asusctl = super.unstable.asusctl;
+          # disable shell completion for dust (completion does not work for me)
+          # - [du-dust nixpkgs](https://github.com/NixOS/nixpkgs/blob/aeefe2054617cae501809b82b44a8e8f7be7cc4b/pkgs/tools/misc/dust/default.nix#L27C1-L27C14)
+          du-dust = super.du-dust.overrideAttrs {postInstall = "";};
         })
       ];
     };
