5392b1afdb8c3c3101790a96fd646f08aa13e299
thrombe <yash.n.thombare620@gmail.com>
2024-04-23 22:38:45 +0530

    kernelParams deep sleep (idk if it even does anything in my case)

--- configma/nixos/etc/nixos/ga402xu/configuration.nix
+++ configma/nixos/etc/nixos/ga402xu/configuration.nix
@@ -181,7 +181,7 @@ in {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # boot.kernelParams = [ "mem_sleep_default=deep" ];
+  boot.kernelParams = ["mem_sleep_default=deep"];
 
   # select patched linux kernel
   # - [Linux kernel nix wiki](https://nixos.wiki/wiki/Linux_kernel)
