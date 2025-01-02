b270aed7a26bde71ae5f7c84c4af321f5cb46512
thrombe <yash.n.thombare620@gmail.com>
2025-01-03 02:35:37 +0530

    use asusctl in eww power script

--- configma/tools/home/.config/eww/scripts/power-interact.sh
+++ configma/tools/home/.config/eww/scripts/power-interact.sh
@@ -1,13 +1,15 @@
 
 contents="$(powerprofilesctl get)"
 
-if [[ "$contents" == "performance" ]]; then
-  powerprofilesctl set balanced
-elif [[ "$contents" == "balanced" ]]; then
-  powerprofilesctl set power-saver
-elif [[ "$contents" == "power-saver" ]]; then
-  powerprofilesctl set performance
-fi
+# if [[ "$contents" == "performance" ]]; then
+#   powerprofilesctl set balanced
+# elif [[ "$contents" == "balanced" ]]; then
+#   powerprofilesctl set power-saver
+# elif [[ "$contents" == "power-saver" ]]; then
+#   powerprofilesctl set performance
+# fi
+
+asusctl profile --next
 
 power="$(./scripts/power.sh)"
 eww update power="$power"
--- configma/tools/home/.config/eww/scripts/power.sh
+++ configma/tools/home/.config/eww/scripts/power.sh
@@ -1,4 +1,7 @@
 
-contents=$(powerprofilesctl get)
+# contents=$(powerprofilesctl get)
+
+contents="$(asusctl profile --profile-get)"
+contents="$(echo "$contents" | tail -n 1 | awk '{print tolower($NF)}')"
 
 echo "{\"profile\": \"$contents\"}"
